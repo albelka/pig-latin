75debb832fa93e74ed2b97fde311b623d0f94b4c
anne belka and bryant wang <bryant@>
2016-10-26 16:09:04 -0700

    Add function processSentence that splits input into an array, checks each object in that array for punctuation and applies the translator function as appropriate, then joins each object together in a new array to be used as output.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// This will apply pig latin rules to the word it's called on.
 function translator (word) {
   if(word.search(/[^aeiou]/i) === 0 && word.search(/[a-z]/i) === 0) {
     console.log("enter first if");
@@ -30,6 +31,52 @@ function translator (word) {
     return word;
   }
 };
+//
+function processSentence (sentence){
+  var arrayOfWords = sentence.split(" ");
+  var arrayOfPigs = [];
+  arrayOfWords.forEach(function(word){
+    console.log("Enters forEach " + word);
+    if(word.search(/\W/) > -1){
+      console.log("enters forEach -> if " + word);
+      // for punctuation in front of word
+      var punctuationFront = "";
+      for (var i = 0; i < word.search(/\w/); i++){
+        if (/\W/g.test(word[i])){
+        punctuationFront += word.slice(i,i+1);
+        word = word.replace(word[0], "");
+
+        i--;
+        }
+      }
+      console.log("after punctuationFront: " + word + "," + punctuationFront);
+      // for punctuation behind word
+      var punctuationEnd = "";
+      for (var i = word.length-1; i > 0; i--){
+        if (/\w/.test(word[i])) {
+          i = 0;
+        } else if (/\W/.test(word[i])){
+        punctuationEnd = word.slice(i,i+1) + punctuationEnd;
+        word = word.replace(word[i], "");
+
+
+        }
+      }
+      console.log("after punctuationEnd: " + word + "," + punctuationEnd);
+      word = translator(word);
+      word = punctuationFront + word + punctuationEnd;
+      arrayOfPigs.push(word);
+      console.log("translated: " + word);
+    } else {
+      console.log("enters forEach -> else " + word);
+      word = translator(word);
+      arrayOfPigs.push(word);
+      console.log("translated: " + word);
+    }
+
+  });
+  return arrayOfPigs.join(" ");
+}
 
 // UI logic under this line
 
@@ -38,7 +85,7 @@ $(function() {
     event.preventDefault();
 
     var sentence = $("#sentence").val();
-    var result = translator(sentence);
+    var result = processSentence(sentence);
     $("#result").text(result);
   });
 });
