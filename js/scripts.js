5989f45fb416b8d2490b0527ca0bceda131062b3
anne belka and bryant wang <bryant@>
2016-10-26 10:09:55 -0700

    Set up form in index.html and add basic ui js.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+
+// UI logic under this line
+
+$(function() {
+  $("form#pig-latin").submit(function(event) {
+    event.preventDefault();
+
+    var sentence = $("#sentence").val();
+    var result = translator(sentence);
+    $("#result").text(result);
+  });
+});
